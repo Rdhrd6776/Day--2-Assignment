54bf60421aadd9fb855a1d72eec3ed3f1ee05780
Radheesh R D <99800128+Rdhrd6776@users.noreply.github.com>
2022-02-20 23:13:28 +0530

    Day - 2 Assignment

---
+++ Day -2 Assignment(HTML).py
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+     
+    </head>
+     
+    <body style="background-color:red;">
+        <h1>Tadani restaurant</h1>
+        <p>A restaurant (sometimes known as a diner) is a place where cooked food is sold to the public, and where people sit down to eat it. It is also a place where people go to enjoy the time and to eat a meal. Some restaurants are a chain, meaning that there are restaurants which have the same name and serve the same food.</p>
+        <div>
+          <img src="blackgoose.png" alt="Black Goose logo" Black Goose Bistro>
+        
+          <img src="/image.pnj" alt="Black Goose logo" Black Goose Bistro>
+        </div>
+    </body>
+</html>
